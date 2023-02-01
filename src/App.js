c23cbad37bb45044e39fa27966f5076d32663f19
Chirag Paul <chiragpaul1989.com>
2023-02-01 23:49:32 +0530

    .

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import React from 'react';
 function App() {
 
   const [realDataArray, setRealDataArray] = React.useState([])
+
+  const [isActiveCard1, setIsActiveCard1] = React.useState(false)
+  const [isActiveCard2, setIsActiveCard2] = React.useState(false)
+
+
+  React.useEffect(()=>{
+    const getData = async()=>{
+    for (let index = 1; index < 5; index++) {
+      const url = `https://api.jikan.moe/v4/top/characters?page=${index}`  
+      const response = await fetch(url)
+      const data = await response.json()
+      const objdata = createDataObjectArray(data)
+      setRealDataArray((prevData) => [...prevData, ...objdata])
+    }
+
+  }
+  getData()
+},[])
  
   function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -14,7 +32,7 @@ function App() {
         array[i] = array[j];
         array[j] = temp;
     }
-}
+  }
 
   function createDataObjectArray(data){
 
@@ -30,41 +48,71 @@ function App() {
     return dataObjectArray
   }
 
-  React.useEffect(()=>{
-      const getData = async()=>{
-      for (let index = 1; index < 5; index++) {
-        const url = `https://api.jikan.moe/v4/top/characters?page=${index}`  
-        const response = await fetch(url)
-        const data = await response.json()
-        const objdata = createDataObjectArray(data)
-        setRealDataArray((prevData) => [...prevData, ...objdata])
-      }
+  function compare(obj1, obj2){
+    if(obj1.favorites>obj2.favorites){
+      return obj1.name
     }
-    getData()
-  },[])
+    else{
+      return obj2.name
+    }
+  }
 
-  let r1 = Math.floor(Math.random() * 50)
-  let r2 = Math.floor(Math.random() * 50)
+  function handleCardClick(e){
+    setIsActiveCard1(current => !current);
+    setIsActiveCard2(current => !current);
+  }
 
+
+  function temp(){
+    console.log('clicked')
+  }
+
+  //Function calling
   shuffleArray(realDataArray)
   console.log(realDataArray)
 
-  return (
-    <div className="App">
-      <Header />
-      <div className='main'>
-        <div className='card-container'>
-          <Card 
-          />
-          <Card />
-        </div>
-        <div className='btn-container'>
-          <button className='btn'>Hello</button>
-          <button className='btn'>World</button>
-        </div>
-      </div>  
-    </div>
-  );
+  if(realDataArray.length<100){
+    return (
+    <p>Loading...</p>
+    )
+  }
+  else{
+
+    let r1 = Math.floor(Math.random() * 50)
+    let r2 = Math.floor(Math.random() * 50)
+
+    let answer = compare(realDataArray[r1], realDataArray[r2])
+
+    return (
+      <div className="App">
+        <Header />
+        <div className='main'>
+          <div className='card-container'>
+            <Card 
+            className = {isActiveCard2?'selected-card':''}
+            name = {realDataArray[r1].name}
+            img = {realDataArray[r1].image}
+            onclick={handleCardClick}
+            />
+            <Card 
+            className = {isActiveCard2?'selected-card':''}
+            name = {realDataArray[r2].name}
+            img = {realDataArray[r2].image}
+            onclick={handleCardClick}
+            />
+          </div>
+          <div className='btn-container'>
+            <button className='btn'
+            
+            >Check!</button>
+          </div>
+          <p className='answer'>{answer}</p>
+        </div>  
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 export default function Card(props){
     return(
-        <div className="card">
+        <div className={props.className} onClick={props.onclick}>
             <div className="image-container">
                 <img src={props.img}
                 className="card-image"></img>
